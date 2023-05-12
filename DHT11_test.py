527e66264887ff64a55d72a7ba71702b953cf0e7
Emily <133243828+Emily-SAC@users.noreply.github.com>
2023-05-12 17:46:53 +0200

    Update DHT11_test.py
    changed quote on quote for bug fix

--- DHT11_test.py
+++ DHT11_test.py
@@ -3,20 +3,20 @@ import time
 
 # Define sensor type and pin number
 sensor = Adafruit_DHT.DHT11
-pin = 16
+pin = 26
 
 # Try to read the sensor
 humidity, temperature = Adafruit_DHT.read(sensor, pin)
 
 # Print results
 if humidity is not None:
-    print('Humidity: {0:0.1f}%'.format(humidity))
+    print("Humidity: {0:0.1f}%".format(humidity))
 else:
-    print('Failed to get humidity reading from DHT11 sensor.')
+    print("Failed to get humidity reading from DHT11 sensor.")
 
 if temperature is not None:
-    print('Temperature: {0:0.1f}*C'.format(temperature))
+    print("Temperature: {0:0.1f}*C".format(temperature))
 else:
-    print('Failed to get temperature reading from DHT11 sensor.');
+    print("Failed to get temperature reading from DHT11 sensor.");
 
 time.sleep(2);
