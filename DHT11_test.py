bdda703d2df1b1fd72e447d5c3ccca79315164e8
Emily <133243828+Emily-SAC@users.noreply.github.com>
2023-05-12 17:03:23 +0200

    Update DHT11_test.py
    tests for temps and humidity separately without a loop

--- DHT11_test.py
+++ DHT11_test.py
@@ -3,21 +3,18 @@ import time
 
 # Define sensor type and pin number
 sensor = Adafruit_DHT.DHT11
-pin = 26
+pin = 16
 
-# Define number of readings
-num_readings = 10
+# Try to read the sensor
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-# Loop through readings
-for i in range(num_readings):
-    # Try to read the sensor
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+# Print results
+if humidity is not None:
+    print('Humidity: {0:0.1f}%'.format(humidity))
+else:
+    print('Failed to get humidity reading from DHT11 sensor.')
 
-    # Print results
-    if humidity is not None and temperature is not None:
-        print('Reading {0}: Temp={1:0.1f}*C  Humidity={2:0.1f}%'.format(i+1, temperature, humidity))
-    else:
-        print('Failed to get reading from DHT11 sensor.')
-
-    # Wait a moment before next reading
-    time.sleep(2)
+if temperature is not None:
+    print('Temperature: {0:0.1f}*C'.format(temperature))
+else:
+    print('Failed to get temperature reading from DHT11 sensor.')
